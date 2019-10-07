5af3bc525e847ade3e10dcbbb77cb4881217e2be
KarthikRaiker <46121872+karthikRaiker@users.noreply.github.com>
2019-10-07 11:06:54 +0530

    Create config.php

---
+++ Demo/config/config.php
@@ -0,0 +1,13 @@
+<?php
+ob_start(); //turns on output buffering
+session_start();
+$con = mysqli_connect("localhost","root","","social1");
+
+$timezone = date_default_timezone_set("Asia/Kolkata");
+
+if(mysqli_connect_errno())
+{
+	echo "failed to connect:" . mysqli_connect_errno();
+}
+
+?>

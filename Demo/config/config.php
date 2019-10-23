91dee353b5af5c84600e603cdacf8ad0d29727df
KarthikRaiker <46121872+karthikRaiker@users.noreply.github.com>
2019-10-24 00:06:07 +0530

    Update config.php

--- Demo/config/config.php
+++ Demo/config/config.php
@@ -1,7 +1,7 @@
 <?php
 ob_start(); //turns on output buffering
 session_start();
-$con = mysqli_connect("localhost","root","","social");
+$con = mysqli_connect("localhost","root","","social1");
 
 $timezone = date_default_timezone_set("Asia/Kolkata");
 

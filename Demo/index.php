478926e2052a2b24b6ddd89effcafb354846eacf
KarthikRaiker <46121872+karthikRaiker@users.noreply.github.com>
2019-11-13 23:06:00 +0530

    Update index.php

--- Demo/index.php
+++ Demo/index.php
@@ -1,8 +1,31 @@
 <?php 
 include("includes/header.php");
-session_destroy();
+
  ?>
-	hello world
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Campus Social Network</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	</head>
+	<body>
+		<section class="profile-sect">
+			<img class="profle" src="<?php echo $user['profile_pic']; ?>">
+			<hr /><br>
+			
+				<p>USERNAME: <?php echo $user['username']; ?></p>
+				<p>EMAIL: <?php echo $user['email']; ?></p>
+				<p>SIGNUP DATE: <?php echo $user['signup_date']; ?></p>
+				<P>NUMBER OF POSTS: <?php echo $user['num_posts']; ?></P>
+			
+		</section>
+	</body>
+	</html>
+
+	
+	
+	
+
 
 </body>
 </html>

3c57aa3b555a18da80332ffc4bb9ad0d59f960e3
Darshan Jali <darshanbhatkal1996@protonmail.com>
2019-12-12 00:49:04 +0530

    uploading profile picture

--- Demo/includes/header.php
+++ Demo/includes/header.php
@@ -1,12 +1,15 @@
-<?php
+<?php  
 require 'config/config.php';
+//include("includes/classes/User.php");
+//include("includes/classes/Post.php");
+
 
 if (isset($_SESSION['username'])) {
-	$userLoggedIn = $_SESSION['username']; 
+	$userLoggedIn = $_SESSION['username'];
 	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
 	$user = mysqli_fetch_array($user_details_query);
 }
-else{
+else {
 	header("Location: register.php");
 }
 
@@ -15,46 +18,61 @@ else{
 <html>
 <head>
 	<title>Campus Social Network</title>
-	<!--java script-->
+
+	<!-- Javascript -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="assets/js/bootstrap.js"></script>
-	<!--css-->
+	<script src="assets/js/bootbox.min.js"></script>
+	<script src="assets/js/demo.js"></script>
+	<script src="assets/js/jquery.jcrop.js"></script>
+	<script src="assets/js/jcrop_bits.js"></script>
+
+
+
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-	<!--font-awesome cdn-->
-	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/css/jquery.Jcrop.css" type="text/css" />
+	
 </head>
 <body>
-	<!---single button-->
-	<div class="top_bar">
+
+	<div class="top_bar"> 
+
 		<div class="logo">
-			<a href="index.php"><img class="xyz" src="assets/images/backgrounds/logo.jpeg"/></a>
+			<a href="index.php">Campus Social Network</a>
 		</div>
+
 		<nav>
 			<a href="<?php echo $userLoggedIn; ?>">
 				<?php echo $user['first_name']; ?>
-			
+			</a>
 			<a href="index.php">
-				<i class="fa fa-home fa-2x" aria-hidden="true"></i>
+				<i class="fa fa-home fa-lg"></i>
 			</a>
 			<a href="#">
-				<i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
+				<i class="fa fa-envelope fa-lg"></i>
 			</a>
 			<a href="#">
-				<i class="fa fa-bell fa-2x" aria-hidden="true"></i>
+				<i class="fa fa-bell fa-lg"></i>
+			</a>
+			<a href="requests.php">
+				<i class="fa fa-users fa-lg"></i>
 			</a>
 			<a href="#">
-				<i class="fa fa-user-plus fa-2x" aria-hidden="true"></i>
-			</a>
-			<a href="#">
-				<i class="fa fa-cogs fa-2x" aria-hidden="true"></i>
+				<i class="fa fa-cog fa-lg"></i>
 			</a>
 			<a href="includes/handlers/logout.php">
-				<i class="fa fa-sign-out fa-2x" aria-hidden="true"></i>
+				<i class="fa fa-sign-out fa-lg"></i>
 			</a>
+
 
 
 		</nav>
+
 	</div>
+
 
 	<div class="wrapper">
--- Demo/profile.php
+++ Demo/profile.php
@@ -3,7 +3,6 @@ include("includes/header.php");
 include("includes/classes/User.php");
 include("includes/classes/Post.php");
 
-
 if(isset($_GET['profile_username'])) {
 	$username = $_GET['profile_username'];
 	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
@@ -71,22 +70,118 @@ if(isset($_POST['respond_request'])) {
 
  			}
 
-
-
  			?>
-
-
  		</form>
+ 		<input type="submit" class="deep_blue" data-toggle="modal" data-target="#post_form" value="Post Something">
+
+    <?php  
+    if($userLoggedIn != $username) {
+      echo '<div class="profile_info_bottom">';
+        echo $logged_in_user_obj->getMutualFriends($username) . " Mutual friends";
+      echo '</div>';
+    }
+
+
+    ?>
 
  	</div>
 
 
-	<div class="main_column column">
-		<?php echo $username; ?>
-		
+	<div class="profile_main_column column">
+		<div class="posts_area"></div>
+    <img id="loading" src="assets/images/icons/loading.gif">
 
 
 	</div>
+
+<!-- Modal -->
+<div class="modal fade" id="post_form" tabindex="-1" role="dialog" aria-labelledby="postModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="postModalLabel">Post something!</h4>
+      </div>
+
+      <div class="modal-body">
+      	<p>This will appear on the user's profile page and also their newsfeed for your friends to see!</p>
+
+      	<form class="profile_post" action="" method="POST">
+      		<div class="form-group">
+      			<textarea class="form-control" name="post_body"></textarea>
+      			<input type="hidden" name="user_from" value="<?php echo $userLoggedIn; ?>">
+      			<input type="hidden" name="user_to" value="<?php echo $username; ?>">
+      		</div>
+      	</form>
+      </div>
+
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" name="post_button" id="submit_profile_post">Post</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<script>
+  var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+  var profileUsername = '<?php echo $username; ?>';
+
+  $(document).ready(function() {
+
+    $('#loading').show();
+
+    //Original ajax request for loading first posts 
+    $.ajax({
+      url: "includes/handlers/ajax_load_profile_posts.php",
+      type: "POST",
+      data: "page=1&userLoggedIn=" + userLoggedIn + "&profileUsername=" + profileUsername,
+      cache:false,
+
+      success: function(data) {
+        $('#loading').hide();
+        $('.posts_area').html(data);
+      }
+    });
+
+    $(window).scroll(function() {
+      var height = $('.posts_area').height(); //Div containing posts
+      var scroll_top = $(this).scrollTop();
+      var page = $('.posts_area').find('.nextPage').val();
+      var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+
+      if ((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
+        $('#loading').show();
+
+        var ajaxReq = $.ajax({
+          url: "includes/handlers/ajax_load_profile_posts.php",
+          type: "POST",
+          data: "page=" + page + "&userLoggedIn=" + userLoggedIn + "&profileUsername=" + profileUsername,
+          cache:false,
+
+          success: function(response) {
+            $('.posts_area').find('.nextPage').remove(); //Removes current .nextpage 
+            $('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage 
+
+            $('#loading').hide();
+            $('.posts_area').append(response);
+          }
+        });
+
+      } //End if 
+
+      return false;
+
+    }); //End (window).scroll(function())
+
+
+  });
+
+  </script>
+
 
 
 

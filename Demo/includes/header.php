60935689ef810d8e900f9a9fee950c8a180e2e0d
Darshan Jali <darshanbhatkal1996@protonmail.com>
2019-12-13 20:42:12 +0530

    notification badge styling

--- Demo/includes/header.php
+++ Demo/includes/header.php
@@ -52,8 +52,9 @@ else {
 			<a href="index.php">
 				<i class="fa fa-home fa-lg"></i>
 			</a>
-			<a href="#">
+			<a href="javascript:void(0);" onclick="getDropdownData('<?php echo $userLoggedIn; ?>', 'message')">
 				<i class="fa fa-envelope fa-lg"></i>
+				
 			</a>
 			<a href="#">
 				<i class="fa fa-bell fa-lg"></i>
@@ -72,7 +73,59 @@ else {
 
 		</nav>
 
+		<div class="dropdown_data_window" style="height:0px; border:none;"></div>
+		<input type="hidden" id="dropdown_data_type" value="">
+
 	</div>
+
+	<script>
+	var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+
+	$(document).ready(function() {
+
+		$('.dropdown_data_window').scroll(function() {
+			var inner_height = $('.dropdown_data_window').innerHeight(); //Div containing data
+			var scroll_top = $('.dropdown_data_window').scrollTop();
+			var page = $('.dropdown_data_window').find('.nextPageDropdownData').val();
+			var noMoreData = $('.dropdown_data_window').find('.noMoreDropdownData').val();
+
+			if ((scroll_top + inner_height >= $('.dropdown_data_window')[0].scrollHeight) && noMoreData == 'false') {
+
+				var pageName; //Holds name of page to send ajax request to
+				var type = $('#dropdown_data_type').val();
+
+
+				if(type == 'notification')
+					pageName = "ajax_load_notifications.php";
+				else if(type = 'message')
+					pageName = "ajax_load_messages.php"
+
+
+				var ajaxReq = $.ajax({
+					url: "includes/handlers/" + pageName,
+					type: "POST",
+					data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+					cache:false,
+
+					success: function(response) {
+						$('.dropdown_data_window').find('.nextPageDropdownData').remove(); //Removes current .nextpage 
+						$('.dropdown_data_window').find('.noMoreDropdownData').remove(); //Removes current .nextpage 
+
+
+						$('.dropdown_data_window').append(response);
+					}
+				});
+
+			} //End if 
+
+			return false;
+
+		}); //End (window).scroll(function())
+
+
+	});
+
+	</script>
 
 
 	<div class="wrapper">
